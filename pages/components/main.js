1f118353501b545f80d9e18deca98145d93f222b
Yuu <anpanmamZ3624@gmail.com>
2022-12-21 14:36:41 +0900

    ファイル名変更

--- pages/components/main.js
+++ pages/components/main.js
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../../styles/Home.module.css";
-import Headline from "./Headline";
+import Headline from "./headline";
 import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
