8f46dfebcf5592636d7885c75f094b535622d59e
Yuu <anpanmamZ3624@gmail.com>
2022-12-29 15:34:12 +0900

    メイン部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -1,10 +1,7 @@
 import styles from "../styles/Home.module.css";
 
 export function Headline(props) {
-  let { title } = props;
-  if (title === "") {
-    title = "Index";
-  }
+  const { title } = props;
   return (
     <div>
       <h1 className={styles.title}>{title} page</h1>
--- components/Main.js
+++ components/Main.js
@@ -1,74 +1,13 @@
-import { useRouter } from "next/router";
-import { Inter } from "@next/font/google";
-import { Headline } from "./Headline";
 import styles from "../styles/Home.module.css";
+import { Headline } from "./Headline";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export function Main() {
-  const router = useRouter();
-  const replaceRouterPathName = router.pathname.replace("/", "");
+export function Main(props) {
+  const { title } = props;
   return (
-    <>
-      <main className={styles.main}>
-        <Headline title={replaceRouterPathName} />
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>Hello</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
+    <main className={styles.main}>
+      <div className={styles.grid}>
+        <Headline title={title} />
+      </div>
+    </main>
   );
 }
