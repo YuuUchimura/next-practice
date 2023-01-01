269dd64f224aaadbccc3b95ca7552ee41b9008f1
Yuu <anpanmamZ3624@gmail.com>
2023-01-02 07:07:50 +0900

    ♻️ スタイルの変更

---
+++ components/Links.js
@@ -0,0 +1,39 @@
+import styles from "../styles/Home.module.css";
+
+const ITEMS = [
+  {
+    href: "https://www.google.com/?hl=ja",
+    title: "Google",
+    description: "Google最高",
+  },
+  {
+    href: "https://www.amazon.co.jp/",
+    title: "Amazon",
+    description: "Amazon最高",
+  },
+  {
+    href: "https://ja-jp.facebook.com/",
+    title: "FaceBook",
+    description: "Facebook良い",
+  },
+  {
+    href: "https://www.apple.com/jp/",
+    title: "Apple",
+    description: "Apple高い",
+  },
+];
+
+export function Links() {
+  return (
+    <div className={styles.cards}>
+      {ITEMS.map((item) => {
+        return (
+          <a key={item.href} href={item.href} className={styles.card}>
+            <div className={styles.title}>{item.title}</div>
+            <div className={styles.description}>{item.description}</div>
+          </a>
+        );
+      })}
+    </div>
+  );
+}
--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,15 @@
 import styles from "../styles/Home.module.css";
 import { Headline } from "./Headline";
+import { Links } from "./Links";
 
 export function Main(props) {
   const { title } = props;
   return (
-    <main className={styles.main}>
-      <div className={styles.grid}>
-        <Headline title={title} />
-      </div>
-    </main>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          <Headline title={title} />
+        </div>
+        <Links />
+      </main>
   );
 }
